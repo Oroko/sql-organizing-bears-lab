79662e2a8997f5f9c486aa7722f8bba773c3d464
Oroko <aoroko09@gmail.com>
2020-12-03 13:59:14 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+create table bears (
+  id INTEGER PRIMARY KEY,
+  name text,
+  age INTEGER,
+  sex text,
+  color text,
+  temperament text,
+  alive INTEGER
+  
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (id,name,age,sex,color,temperament,alive) values
+(1,"abe",2,"male","brown","calm",0),
+(2,"june",4,"female","white","aggressive",1),
+(3,"elio",1,"male","black","relaxed",1),
+(4,"maxi",20,"female","green","sad",0),
+(5,"zio",34,"female","brown","happy",1),
+(6,"aella",16,"male","white","calm",1),
+(7,"truman",12,"female","brown","aggressive",1),
+(8,NULL,10,"male","black","sad", 0);
